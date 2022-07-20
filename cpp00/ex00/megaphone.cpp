feacc7435d47685e56338f5c48e8499ef228eb3e
Joko Pfuhl <jpfuhl@2-B-4.42heilbronn.de>
2022-07-20 21:00:48 +0200

    updated megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,12 +4,7 @@
 static void	print_uppercase(char *str)
 {
 	for (int i = 0; str[i] != '\0'; i++)
-	{
-		if (islower(str[i]))
-			std::cout << (char)(str[i] - 32);
-		else
-			std::cout << str[i];
-	}
+		std::cout << (char)toupper(str[i]);
 }
 
 int	main(int argc, char **argv)
