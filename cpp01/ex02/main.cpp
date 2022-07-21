35029d36640c823c25e890d4648cb0b36720a552
Joko Pfuhl <jpfuhl@3-B-1.42heilbronn.de>
2022-07-21 20:34:34 +0200

    add ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/21 20:05:20 by jpfuhl            #+#    #+#             */
+/*   Updated: 2022/07/21 20:25:55 by jpfuhl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string	&stringREF = brain;
+
+	std::cout << "Memory Address:" << std::endl;
+	std::cout << "STR: " << &brain << std::endl;
+	std::cout << "PTR: " << stringPTR << std::endl;
+	std::cout << "REF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Value:" << std::endl;
+	std::cout << "STR: " << brain << std::endl;
+	std::cout << "PTR: " << *stringPTR << std::endl;
+	std::cout << "REF: " << stringREF << std::endl;
+}
