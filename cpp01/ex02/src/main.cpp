f41489f247ed0b6b40ecd944ccd601b4649b19f4
Joko Pfuhl <jpfuhl@2-B-4.42heilbronn.de>
2022-07-22 19:10:05 +0200

    added ref example

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 20:05:20 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/07/21 20:25:55 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/07/22 15:21:42 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@ int	main(void)
 	std::cout << std::endl;
 
 	std::cout << "Value:" << std::endl;
+	std::cout << "STR: " << brain << std::endl;
+	std::cout << "PTR: " << *stringPTR << std::endl;
+	std::cout << "REF: " << stringREF << std::endl;
+	std::cout << std::endl;
+
+	stringREF = "THIS IS NO LONGER BRAIN";
+	std::cout << "Change stringREF:" << std::endl;
 	std::cout << "STR: " << brain << std::endl;
 	std::cout << "PTR: " << *stringPTR << std::endl;
 	std::cout << "REF: " << stringREF << std::endl;
