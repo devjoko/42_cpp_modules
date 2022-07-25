cd6467fb3ae1ca2e90e2d81a490c6aef0f4823d8
Joko Pfuhl <jpfuhl@2-C-7.42heilbronn.de>
2022-07-25 22:57:53 +0200

    added ex01, reworked ex00 a bit -- operation overload confusion still :D

--- cpp02/ex01/inc/Fixed.hpp
+++ cpp02/ex01/inc/Fixed.hpp
@@ -6,13 +6,15 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:55:24 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/07/25 22:39:08 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/07/25 22:55:16 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef FIXED_H
 # define FIXED_H
+
+# include <iostream>
 
 /* ************************************************************************** */
 /*                                 Fixed Class                                */
@@ -25,13 +27,19 @@ private:
 	int const static	fractionalBits_;
 public:
 	Fixed();
+	Fixed( int const i );
+	Fixed( float const f );
 	Fixed( Fixed const &src );
 	~Fixed();
 
-	Fixed	&operator=( Fixed const &src);
+	Fixed	&operator=( Fixed const &src );
 
+	float	toFloat ( void ) const;
+	int		toInt ( void ) const;
 	int		getRawBits( void ) const;
 	void	setRawBits( int const raw );
 };
+
+std::ostream	&operator<<( std::ostream &o, Fixed const &src );
 
 #endif /* FIXED_H */
--- cpp02/ex01/src/Fixed.cpp
+++ cpp02/ex01/src/Fixed.cpp
@@ -6,19 +6,43 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 20:40:11 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/07/25 22:35:26 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/07/25 22:55:42 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <cmath>
 #include "../inc/Fixed.hpp"
 
+/* ************************************************************************** */
+/*                              Static Declarations                           */
+/* ************************************************************************** */
+
 int const	Fixed::fractionalBits_ = 8;
+
+/* ************************************************************************** */
+/*                                 Fixed Class                                */
+/* ************************************************************************** */
 
 /*	DEFAULT CONSTRUCTOR	*/
 Fixed::Fixed() : rawBits_( 0 )
 {
 	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+/*	PARAMETRIC CONSTRUCTOR (INTEGER)	*/
+Fixed::Fixed( int const i )
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->rawBits_ = i << this->fractionalBits_; // ??? What ???
+	return ;
+}
+
+/*	PARAMETRIC CONSTRUCTOR (FLOATING-POINT)	*/
+Fixed::Fixed( float const f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->rawBits_ = roundf(f * (1 << this->fractionalBits_)); // ??? WHAT ???
 	return ;
 }
 
@@ -46,10 +70,20 @@ Fixed&	Fixed::operator=( Fixed const &src )
 	return (*this);
 }
 
+/*	PUBLIC MEMBER FUNCTIONS	*/
+float	Fixed::toFloat( void ) const
+{
+	return ((float)this->rawBits_ / (float)(1 << this->fractionalBits_)); // ???
+}
+
+int	Fixed::toInt( void ) const
+{
+	return (this->rawBits_ >> this->fractionalBits_); // ???
+}
+
 /*	GETTER	*/
 int	Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->rawBits_);
 }
 
@@ -57,4 +91,14 @@ int	Fixed::getRawBits( void ) const
 void	Fixed::setRawBits( int const raw)
 {
 	this->rawBits_ = raw;
+}
+
+/* ************************************************************************** */
+/*                               Non-Member Functions                         */
+/* ************************************************************************** */
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &src) // ???
+{
+	o << src.toFloat();
+	return (o);
 }
