820110b157787066dea79b984197c0ba621e414f
Joko Pfuhl <jpfuhl@3-C-1.42heilbronn.de>
2022-07-28 19:38:16 +0200

    added fragtrap ex02

---
+++ cpp03/ex02/inc/ClapTrap.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 19:27:58 by jpfuhl            #+#    #+#             */
+/*   Updated: 2022/07/28 18:35:08 by jpfuhl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <iostream>
+# include <string>
+
+/* ************************************************************************** */
+/*                                 ClapTrap Class                             */
+/* ************************************************************************** */
+
+class	ClapTrap
+{
+protected:
+	std::string		_name;
+	unsigned int	_hitpoints;
+	unsigned int	_energy;
+	unsigned int	_damage;
+	unsigned int	_maxHitpoints;
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &copy);
+	ClapTrap	&operator=(const ClapTrap &assign);
+	~ClapTrap();
+
+	void	attack(const std::string &target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+	std::string		getName(void) const;
+	unsigned int	getHitpoints(void) const;
+	unsigned int	getEnergy(void) const;
+	unsigned int	getDamage(void) const;
+	unsigned int	getMaxHitpoints(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &o, const ClapTrap &obj);
+
+#endif /* CLAPTRAP_H */
---
+++ cpp03/ex02/src/main.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 01:03:26 by jpfuhl            #+#    #+#             */
+/*   Updated: 2022/07/28 19:33:02 by jpfuhl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ScavTrap.hpp"
+#include "../inc/FragTrap.hpp"
+
+int	main(void)
+{
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << "/*                      Let's show inheritance from ClapTrap.                 */" << std::endl;
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << std::endl;
+	{
+		{
+			ClapTrap	clap("CL4P-TP");
+			std::cout << clap;
+		}
+		std::cout << "/* ************************************************************************** */" << std::endl;
+		std::cout << std::endl;
+		{
+			ScavTrap	scav("SC4V-TP");
+			std::cout << scav;
+		}
+		std::cout << "/* ************************************************************************** */" << std::endl;
+		std::cout << std::endl;
+		{
+			FragTrap	frag("FR4G-TP");
+			std::cout << frag;
+		}
+	}
+
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << "/*                          Let's oneshot our fragtrap.                       */" << std::endl;
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << std::endl;
+	{
+		FragTrap	frag("FR4G-TP");
+
+		std::cout << frag;
+		frag.takeDamage(4294967290);
+		std::cout << frag;
+		frag.beRepaired(10);
+	}
+
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << "/*                                Let's make it fight!                        */" << std::endl;
+	std::cout << "/* ************************************************************************** */" << std::endl;
+	std::cout << std::endl;
+	{
+		FragTrap	frag("FR4G-TP");
+
+		std::cout << frag;
+		frag.highFivesGuys();
+		frag.attack("Badass");
+		frag.takeDamage(98);
+		std::cout << frag;
+		frag.beRepaired(100);
+		frag.beRepaired(1);
+		frag.highFivesGuys();
+		std::cout << frag;
+	}
+
+	return (0);
+}
