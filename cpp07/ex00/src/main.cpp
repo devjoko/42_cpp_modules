979cb16a6207d2fba9cf6542ead1f4c7f8007f3e
Joko Pfuhl <jpfuhl@2-A-5.42heilbronn.de>
2022-09-26 19:31:56 +0200

    <cpp07::ex00> : added many test cases to main function

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -6,29 +6,123 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 01:45:49 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/09/07 01:56:57 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/09/26 19:29:55 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/whatever.hpp"
 
+#define YELLOW	"\033[1;33m"
+#define BLUE	"\033[1;36m"
+#define END		"\033[0m"
+
 int main(void)
 {
-	int a = 2;
-	int b = 3;
 
-	::swap(a, b);
+
+{
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::cout << YELLOW << "/*                              Testing Integers                              */" << END << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	int a = 1;
+	int b = 100;
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::cout << BLUE << "* SWAP! *" << END << std::endl;
+	::swap(a,b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
 
-	return (0);
+	std::cout << BLUE << "* MIN! *" << END << std::endl;
+	std::cout << ::min(a,b) << std::endl;
+
+	std::cout << BLUE << "* MAX! *" << END << std::endl;
+	std::cout << ::max(a,b) << std::endl;
+}
+{
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::cout << YELLOW << "/*                             Testing Strings                                */" << END << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::string a = "Hello World!";
+	std::string b = "Goodbye World!";
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << BLUE << "* SWAP! *" << END << std::endl;
+	::swap(a,b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+
+	std::cout << BLUE << "* MIN! *" << END << std::endl;
+	std::cout << ::min(a,b) << std::endl;
+
+	std::cout << BLUE << "* MAX! *" << END << std::endl;
+	std::cout << ::max(a,b) << std::endl;
+}
+{
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::cout << YELLOW << "/*                             Testing Floats                                 */" << END << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	float a = 1.23f;
+	float b = 9.87f;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << BLUE << "* SWAP! *" << END << std::endl;
+	::swap(a,b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+
+	std::cout << BLUE << "* MIN! *" << END << std::endl;
+	std::cout << ::min(a,b) << std::endl;
+
+	std::cout << BLUE << "* MAX! *" << END << std::endl;
+	std::cout << ::max(a,b) << std::endl;
+}
+{
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::cout << YELLOW << "/*                             Testing Chars                                  */" << END << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	char a = 'A';
+	char b = 'Z';
+	std::cout << "1 = " << a << ", 2 = " << b << std::endl;
+
+	std::cout << BLUE << "* SWAP! *" << END << std::endl;
+	::swap(a,b);
+	std::cout << "1 = " << a << ", 2 = " << b << std::endl;
+
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+
+	std::cout << BLUE << "* MIN! *" << END << std::endl;
+	std::cout << ::min(a,b) << std::endl;
+
+	std::cout << BLUE << "* MAX! *" << END << std::endl;
+	std::cout << ::max(a,b) << std::endl;
+}
+{
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	std::cout << YELLOW << "/*                             Testing Classes                                */" << END << std::endl;
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+	Awesome a(1);
+	Awesome b(9);
+	std::cout << "a = " << a << "b = " << b;
+
+	std::cout << BLUE << "* SWAP! *" << END << std::endl;
+	::swap(a,b);
+	std::cout << "a = " << a << "b = " << b;
+
+	std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+
+	std::cout << BLUE << "* MIN! *" << END << std::endl;
+	Awesome minimum = ::min(a,b);
+	std::cout << minimum;
+
+	std::cout << BLUE << "* MAX! *" << END << std::endl;
+	Awesome maximum = ::max(a,b);
+	std::cout << maximum;
+}
+
+
+return (0);
 }
