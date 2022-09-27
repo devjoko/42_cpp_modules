7d5b89cc169f57e3a11a330e0d3d3c9fb1214bdb
Joko Pfuhl <jpfuhl@2-A-5.42heilbronn.de>
2022-09-27 04:04:07 +0200

    <cpp08::ex02> : completed and improved MutantStack.hpp; added main test case to show list and stack behaviour; added Makefile symbol

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -6,9 +6,15 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 18:45:06 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/09/09 19:12:58 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/09/27 03:42:24 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** STACK is a "container adaptor" and is constructed by taking the sequential container DEQUE
+** and "adapting" (restricting/modifying) its interface to provide the desired behaviour.
+** It adapts the deque container to provide strict last-in, first-out (LIFO) behaviour.
+*/
 
 #ifndef MUTANT_STACK_H
 # define MUTANT_STACK_H
@@ -16,33 +22,38 @@
 # include <iostream>
 # include <stack>
 
+#define	RED			"\033[1;31m"
+#define	GREEN		"\033[1;32m"
+#define YELLOW		"\033[1;33m"
+#define BLUE		"\033[1;36m"
+#define	END			"\033[0m"
+
 template<typename T>
 class MutantStack : public std::stack<T>
 {
-private:
 public:
-	typedef typename std::stack<T>::container_type::iterator it;
-	MutantStack()
+
+	/* MEMBER TYPES */
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+//	STACK has access to the underlying container_type c, which is DEQUE
+//	protected:
+//	container_type c
+
+public:
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack& rhs) : std::stack<T>(rhs) {}
+	~MutantStack() {}
+
+	MutantStack& operator=(const MutantStack& rhs)
 	{
-		std::cout << "ctor" << std::endl;
-	}
-	MutantStack(const MutantStack& rhs)
-	{
-		*this = rhs;
-	}
-	~MutantStack()
-	{
-		std::cout << "dtor" << std::endl;
-	}
-	it begin(void)
-	{
-		return (this->c.begin());
+		if (this != &rhs)
+			std::stack<T>::operator=(rhs);
+		return (*this);
 	}
 
-	it end(void)
-	{
-		return (this->c.end());
-	}
+	iterator begin(void) { return ( std::stack<T>::c.begin() ); }
+	iterator end(void) { return ( std::stack<T>::c.end() ); }
 };
 
 #endif /* MUTANT_STACK_H */
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -6,41 +6,139 @@
 /*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 18:57:01 by jpfuhl            #+#    #+#             */
-/*   Updated: 2022/09/09 19:04:53 by jpfuhl           ###   ########.fr       */
+/*   Updated: 2022/09/27 04:02:57 by jpfuhl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/MutantStack.hpp"
+#include <list>
 
-int main(void)
+void printList(std::list<int> list)
 {
-	MutantStack<int> mstack;
-	
-	mstack.push(5);
-	mstack.push(17);
-	
-	std::cout << mstack.top() << std::endl;
-	
-	mstack.pop();
+	std::list<int>::iterator it = list.begin();
+	std::list<int>::iterator ite = list.end();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << GREEN << "size() : " << list.size() << END << std::endl;
 
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...] mstack.push(0);
+	if (list.size() > 0)
+		std::cout << GREEN << "top() : " << list.back() << END << std::endl;
 
+	std::cout << GREEN << "Stack : ";
+	while (it != ite)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << END << std::endl;
+}
+
+void printStack(MutantStack<int> mstack)
+{
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
-	++it;
-	--it;
+	std::cout << GREEN << "size() : " << mstack.size() << END << std::endl;
 
+	if (mstack.size() > 0)
+		std::cout << GREEN << "top() : " << mstack.top() << END << std::endl;
+
+	std::cout << GREEN << "Stack : ";
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 		++it;
 	}
+	std::cout << END << std::endl;
+}
+
+int main(void)
+{
+
+
+std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+std::cout << YELLOW << "/*                                  Mutant Stack                              */" << END << std::endl;
+std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+{
+	MutantStack<int> mstack;
+
+	printStack(mstack);
+
+	std::cout << BLUE << "* adding numbers to stack *" << END << std::endl;
+	mstack.push(5);
+	mstack.push(17);
+
+	printStack(mstack);
+
+	std::cout << BLUE << "* pop() number off stack *" << END << std::endl;
+	mstack.pop();
+
+	printStack(mstack);
+
+	std::cout << BLUE << "* adding more numbers to stack *" << END << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...] 
+	mstack.push(0);
+
+	printStack(mstack);
+
+	std::cout << BLUE << "* pop() number off stack TWICE *" << END << std::endl;
+	mstack.pop();
+	mstack.pop();
+
+	printStack(mstack);
+
+	std::cout << BLUE << "* iterator modification for fun *" << END << std::endl;
+	MutantStack<int>::iterator it = mstack.begin();
+	++it;
+	--it;
+	std::cout << BLUE << "* copy constructing for fun *" << END << std::endl;
 	std::stack<int> s(mstack);
-	return (0);
+	std::cout << BLUE << "* copy assigning for fun *" << END << std::endl;
+	std::stack<int> copy;
+	copy = s;
+
+	// system("leaks mutantstack");
+}
+std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+std::cout << YELLOW << "/*                                      List                                  */" << END << std::endl;
+std::cout << YELLOW << "/* ************************************************************************** */" << END << std::endl;
+{
+	std::list<int> list;
+
+	printList(list);
+
+	std::cout << BLUE << "* adding numbers to list *" << END << std::endl;
+	list.push_back(5);
+	list.push_back(17);
+	
+	printList(list);
+	
+	std::cout << BLUE << "* pop() number off list *" << END << std::endl;
+	list.pop_back();
+
+	printList(list);
+
+	std::cout << BLUE << "* adding more numbers to list *" << END << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	//[...]
+	list.push_back(0);
+
+	printList(list);
+
+	std::cout << BLUE << "* pop() number off from both sides *" << END << std::endl;
+	list.pop_front();
+	list.pop_back();
+
+	printList(list);
+
+	std::cout << RED << "* see the difference between <-list-> and stack-> *" << END << std::endl;
+}
+return (0);
+
+
+
 }
